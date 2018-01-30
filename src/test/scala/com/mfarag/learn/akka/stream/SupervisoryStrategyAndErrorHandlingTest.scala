d70411f282e08247a15e807b5d82ea8307673ef4
Muhammad Farag <MuhammadFarag@Gmail.com>
2018-01-30 15:51:56 -0500

    graph results in Either to represent errors

--- src/test/scala/com/mfarag/learn/akka/stream/SupervisoryStrategyAndErrorHandlingTest.scala
+++ src/test/scala/com/mfarag/learn/akka/stream/SupervisoryStrategyAndErrorHandlingTest.scala
@@ -11,8 +11,9 @@ import org.scalatest.{FunSuiteLike, Matchers}
 
 import scala.collection.immutable
 import scala.concurrent.Future
+import scala.util.{Success, Try}
 
-class SupervisoryStrategyTest extends TestKit(ActorSystem("test-system")) with FunSuiteLike with StopSystemAfterAll with Matchers with ScalaFutures {
+class SupervisoryStrategyAndErrorHandlingTest extends TestKit(ActorSystem("test-system")) with FunSuiteLike with StopSystemAfterAll with Matchers with ScalaFutures {
 
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   val parse: Flow[String, Int, NotUsed] = Flow[String].map(_.toInt)
@@ -96,6 +97,27 @@ class SupervisoryStrategyTest extends TestKit(ActorSystem("test-system")) with F
 
     whenReady(graph(parse).run) { result =>
       result shouldBe Seq(1, 2, 4)
+    }
+  }
+
+  test("graph results in Either to represent errors") {
+    val errorReportingParse: Flow[String, Either[String, Int], NotUsed] =
+      Flow[String].map(numeric =>
+        Try {
+          numeric.toInt
+        } match {
+          case Success(v) => Right(v)
+          case _ => Left(numeric)
+        })
+
+    whenReady(
+      Source.single(ByteString("1,2,a,4"))
+        .via(Framing
+          .delimiter(ByteString(","), 3, allowTruncation = true)
+          .map(_.decodeString("UTF8")))
+        .via(errorReportingParse).toMat(Sink.seq)(Keep.right)
+        .run) { result =>
+      result shouldBe Seq(Right(1), Right(2), Left("a"), Right(4))
     }
   }
 
